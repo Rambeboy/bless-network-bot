d4f529bacd67f271ef83c5e638baa6f1da61b0c6
R4MB3B0Y <kingindoslayer@gmail.com>
2024-11-20 00:31:22 +0700

    Update config.js

--- config/config.js
+++ config/config.js
@@ -2,21 +2,21 @@ module.exports = [
     {
         usertoken: 'INPUT YOUR TOKEN HERE 1',
         nodes: [
-            { nodeId: 'INPUT YOUR NODEID HERE 1',  hardwareId: 'INPUT YOUR HARDWAREID 1',  proxy: 'INPUT YOUR PROXY 1' },
-            { nodeId: 'INPUT YOUR NODEID HERE 2',  hardwareId: 'INPUT YOUR HARDWAREID 2',  proxy: 'INPUT YOUR PROXY 2' },
-            { nodeId: 'INPUT YOUR NODEID HERE 3',  hardwareId: 'INPUT YOUR HARDWAREID 3',  proxy: 'INPUT YOUR PROXY 3' },
-            { nodeId: 'INPUT YOUR NODEID HERE 4',  hardwareId: 'INPUT YOUR HARDWAREID 4',  proxy: 'INPUT YOUR PROXY 4' },
-            { nodeId: 'INPUT YOUR NODEID HERE 5',  hardwareId: 'INPUT YOUR HARDWAREID 5',  proxy: 'INPUT YOUR PROXY 5' }
+            { nodeId: 'INPUT YOUR NODEID HERE 1',  hardwareId: 'INPUT YOUR HARDWAREID HERE 1',  proxy: 'INPUT YOUR PROXY HERE 1' },
+            { nodeId: 'INPUT YOUR NODEID HERE 2',  hardwareId: 'INPUT YOUR HARDWAREID HERE 2',  proxy: 'INPUT YOUR PROXY HERE 2' },
+            { nodeId: 'INPUT YOUR NODEID HERE 3',  hardwareId: 'INPUT YOUR HARDWAREID HERE 3',  proxy: 'INPUT YOUR PROXY HERE 3' },
+            { nodeId: 'INPUT YOUR NODEID HERE 4',  hardwareId: 'INPUT YOUR HARDWAREID HERE 4',  proxy: 'INPUT YOUR PROXY HERE 4' },
+            { nodeId: 'INPUT YOUR NODEID HERE 5',  hardwareId: 'INPUT YOUR HARDWAREID HERE 5',  proxy: 'INPUT YOUR PROXY HERE 5' }
         ]
     },
     {
         usertoken: 'INPUT YOUR TOKEN HERE 2',
         nodes: [
-            { nodeId: 'INPUT YOUR NODEID HERE 6',  hardwareId: 'INPUT YOUR HARDWAREID 6',  proxy: 'INPUT YOUR PROXY 6' },
-            { nodeId: 'INPUT YOUR NODEID HERE 7',  hardwareId: 'INPUT YOUR HARDWAREID 7',  proxy: 'INPUT YOUR PROXY 7' },
-            { nodeId: 'INPUT YOUR NODEID HERE 8',  hardwareId: 'INPUT YOUR HARDWAREID 8',  proxy: 'INPUT YOUR PROXY 8' },
-            { nodeId: 'INPUT YOUR NODEID HERE 9',  hardwareId: 'INPUT YOUR HARDWAREID 9',  proxy: 'INPUT YOUR PROXY 9' },
-            { nodeId: 'INPUT YOUR NODEID HERE 10', hardwareId: 'INPUT YOUR HARDWAREID 10', proxy: 'INPUT YOUR PROXY 10' }
+            { nodeId: 'INPUT YOUR NODEID HERE 6',  hardwareId: 'INPUT YOUR HARDWAREID HERE 6',  proxy: 'INPUT YOUR PROXY HERE 6' },
+            { nodeId: 'INPUT YOUR NODEID HERE 7',  hardwareId: 'INPUT YOUR HARDWAREID HERE 7',  proxy: 'INPUT YOUR PROXY HERE 7' },
+            { nodeId: 'INPUT YOUR NODEID HERE 8',  hardwareId: 'INPUT YOUR HARDWAREID HERE 8',  proxy: 'INPUT YOUR PROXY HERE 8' },
+            { nodeId: 'INPUT YOUR NODEID HERE 9',  hardwareId: 'INPUT YOUR HARDWAREID HERE 9',  proxy: 'INPUT YOUR PROXY HERE 9' },
+            { nodeId: 'INPUT YOUR NODEID HERE 10', hardwareId: 'INPUT YOUR HARDWAREID HERE 10', proxy: 'INPUT YOUR PROXY HERE 10' }
         ]
     }
 ];
