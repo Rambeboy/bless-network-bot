81d8158e1d7c07c737a2f6b87c8a738888b0ac19
R4MB3B0Y <kingindoslayer@gmail.com>
2024-11-20 00:29:19 +0700

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,23 +1,22 @@
 module.exports = [
     {
-        usertoken: 'usertoken1',
+        usertoken: 'INPUT YOUR TOKEN HERE 1',
         nodes: [
-            { nodeId: 'nodeid1', hardwareId: 'hardwareid1', proxy: 'proxy1' },
-            { nodeId: 'nodeid2', hardwareId: 'hardwareid2', proxy: 'proxy2' },
-            { nodeId: 'nodeid3', hardwareId: 'hardwareid3', proxy: 'proxy3' },
-            { nodeId: 'nodeid4', hardwareId: 'hardwareid4', proxy: 'proxy4' },
-            { nodeId: 'nodeid5', hardwareId: 'hardwareid5', proxy: 'proxy5' }
+            { nodeId: 'INPUT YOUR NODEID HERE 1',  hardwareId: 'INPUT YOUR HARDWAREID 1',  proxy: 'INPUT YOUR PROXY 1' },
+            { nodeId: 'INPUT YOUR NODEID HERE 2',  hardwareId: 'INPUT YOUR HARDWAREID 2',  proxy: 'INPUT YOUR PROXY 2' },
+            { nodeId: 'INPUT YOUR NODEID HERE 3',  hardwareId: 'INPUT YOUR HARDWAREID 3',  proxy: 'INPUT YOUR PROXY 3' },
+            { nodeId: 'INPUT YOUR NODEID HERE 4',  hardwareId: 'INPUT YOUR HARDWAREID 4',  proxy: 'INPUT YOUR PROXY 4' },
+            { nodeId: 'INPUT YOUR NODEID HERE 5',  hardwareId: 'INPUT YOUR HARDWAREID 5',  proxy: 'INPUT YOUR PROXY 5' }
         ]
     },
     {
-        usertoken: 'usertoken2',
+        usertoken: 'INPUT YOUR TOKEN HERE 2',
         nodes: [
-            { nodeId: 'nodeid6', hardwareId: 'hardwareid6', proxy: 'proxy6' },
-            { nodeId: 'nodeid7', hardwareId: 'hardwareid7', proxy: 'proxy7' },
-            { nodeId: 'nodeid8', hardwareId: 'hardwareid8', proxy: 'proxy8' },
-            { nodeId: 'nodeid9', hardwareId: 'hardwareid9', proxy: 'proxy9' },
-            { nodeId: 'nodeid10', hardwareId: 'hardwareid10', proxy: 'proxy10' }
+            { nodeId: 'INPUT YOUR NODEID HERE 6',  hardwareId: 'INPUT YOUR HARDWAREID 6',  proxy: 'INPUT YOUR PROXY 6' },
+            { nodeId: 'INPUT YOUR NODEID HERE 7',  hardwareId: 'INPUT YOUR HARDWAREID 7',  proxy: 'INPUT YOUR PROXY 7' },
+            { nodeId: 'INPUT YOUR NODEID HERE 8',  hardwareId: 'INPUT YOUR HARDWAREID 8',  proxy: 'INPUT YOUR PROXY 8' },
+            { nodeId: 'INPUT YOUR NODEID HERE 9',  hardwareId: 'INPUT YOUR HARDWAREID 9',  proxy: 'INPUT YOUR PROXY 9' },
+            { nodeId: 'INPUT YOUR NODEID HERE 10', hardwareId: 'INPUT YOUR HARDWAREID 10', proxy: 'INPUT YOUR PROXY 10' }
         ]
     }
-    // Add more usertokens as needed
 ];
